cfe1b4c7ca65d7f50894198d6a126a9765fcf804
Mallory Smith <mallorysmith6464@gmail.com>
2019-08-13 16:56:53 -0400

    api up and running

--- public/main.js
+++ public/main.js
@@ -1,7 +1,19 @@
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  const input = document.querySelector('input').value
+  const url = 'https://api.openweathermap.org/data/2.5/'
+  const key = '2823a9a190fd597e6803d812db33835c'
+  const location = `${url}weather?q=${input}&appid=${key}`
+  fetch(location)
+    .then(response => {
+      console.log(response)
+      return response.json()
+    })
+    .then(city => {
+      console.log(city)
+      document.querySelector('.city').textContent = city.main.temp
+      document.querySelector('.humidity').textContent = city.main.humidity
+      document.querySelector('.wind').textContent = city.wind.speed
+    })
 }
 
-document.addEventListener('DOMContentLoaded', main)
+document.querySelector('button').addEventListener('click', main)
