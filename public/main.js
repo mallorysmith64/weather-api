fdd37b07d295cb508c46246cda9307b57719a2d5
Mallory Smith <mallorysmith6464@gmail.com>
2019-08-13 21:00:39 -0400

    added geolocation

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-const main = () => {
+const getWeather = () => {
   const input = document.querySelector('input').value
   const url = 'https://api.openweathermap.org/data/2.5/'
   const key = '2823a9a190fd597e6803d812db33835c'
@@ -12,27 +12,13 @@ const main = () => {
       console.log(city)
       document.querySelector('.temp').textContent = city.main.temp
       document.querySelector('.humidity').textContent = city.main.humidity
+      document.querySelector('.pressure').textContent = city.main.pressure
       document.querySelector('.wind-speed').textContent = city.wind.speed
     })
 }
 //todo: add geolocation
-// const getLocation = () => {
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(geoSuccess, geoError)
-//   } else {
-//     alert('Geolocation is not supported by this browser.')
-//   }
-// }
+navigator.geolocation.getCurrentPosition(function(position) {
+  console.log(position.coords.latitude, position.coords.longitude)
+})
 
-// const geoSuccess = position => {
-//   let lat = position.coords.latitude
-//   let lng = position.coords.longitude
-//   alert('lat:' + lat + ' lng:' + lng)
-// }
-
-// const geoError = () => {
-//   alert('Geocoder failed.')
-// }
-
-document.querySelector('button').addEventListener('click', main)
-// document.addEventListener('DOMContentLoaded', getLocation)
+document.querySelector('button').addEventListener('click', getWeather)
